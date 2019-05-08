47aa2071e9ccefbc53b879d47ef4746eff4d7636
vkotaru <prasanth.kotaru@gmail.com>
2019-05-08 10:41:11 -0700

    [update] tmp test cases

---
+++ spinup/examples/HumanoidStandup_v2.py
@@ -0,0 +1,24 @@
+from spinup.utils.run_utils import ExperimentGrid
+from spinup import ppo
+import tensorflow as tf
+import gym
+import os
+# path = '/home/kotaru/workspace/git/third_party/gym-cassie'
+# os.environ['PATH'] += ':'+path
+import gym_cassie
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--num_runs', type=int, default=3)
+    args = parser.parse_args()
+
+    eg = ExperimentGrid(name='ppo-bench')
+    eg.add('env_name', 'HumanoidStandup-v2', '', True)
+    eg.add('seed', [0])
+    eg.add('epochs', 10)
+    eg.add('steps_per_epoch', 4000)
+    eg.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
+    eg.add('ac_kwargs:activation', [tf.tanh, tf.nn.relu], '')
+    eg.run(ppo, num_cpu=args.cpu)
